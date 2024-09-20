6bd75f19abdf92849b0f4fdbb47831cfb7e80079
apaulinobatista26 <amauryp13@nycstudents.net>
2024-09-20 12:07:06 -0400

    challenge 1 complete

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -10,6 +10,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -8);
+        transform.position = player.transform.position + offset;
     }
 }
