a120fdba71a7b2bba50d6b4699aeb87dce0b5c0d
apaulinobatista26 <amauryp13@nycstudents.net>
2025-03-18 12:39:21 -0400

    mini challenge completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {     
-    public float speed = 5.0f;
     public float turnspeed = 50.0f;
-    float horizontalInput;
+    public float speed = 5.0f;
+    public float horizontalInput;
     float forwardInput;
+    public GameObject thirdPersonCamera;
+    public GameObject firstPersonCamera;
+    private bool isFirstPerson = false;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thirdPersonCamera.SetActive(true);
+        firstPersonCamera.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +27,18 @@ public class PlayerController : MonoBehaviour
        // Move vechile forward
        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+
+
+        if (Input.GetKeyDown(KeyCode.C)) 
+        {
+           isFirstPerson = !isFirstPerson; 
+           thirdPersonCamera.SetActive(!isFirstPerson); 
+           firstPersonCamera.SetActive(isFirstPerson);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+            // Launch a projectile from player 
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+         }
     }
 }
